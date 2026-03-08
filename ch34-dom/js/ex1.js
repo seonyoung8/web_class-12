7907034d4bbce0af8e9a9a77effd224215ce5860
seonyoung <ysr00317@gmail.com>
2026-03-09 08:19:50 +0900

    ch34 예제이어서 하면 됨

---
+++ ch34-dom/js/ex1.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(){
+    // 문서 끝까지 읽고 난 뒤 한번 실행되는 소스코드
+    const btnClosePopup = document.querySelector(".btn-close-popup")
+    const popup = document.querySelector(".popup")
+    btnClosePopup.addEventListener("click",()=>{
+        popup.classList.add("hide")
+    })
+})
