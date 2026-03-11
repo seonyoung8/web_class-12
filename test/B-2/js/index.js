bc1ebaef9b764c48181ac06bb0642f5a85d6a009
김선영 <ysr00317@gmail.com>
2026-03-11 18:09:28 +0900

    B-2풀이했음

--- test/B-2/js/index.js
+++ test/B-2/js/index.js
@@ -7,14 +7,13 @@ document.addEventListener("DOMContentLoaded",()=>{
     const btnClose = document.querySelector("#btn-close")
 
     btnPopup.addEventListener("click",()=>{
-        // 팝업열리는기능
-        popup.classList.add("on")
         modal.classList.add("on")
+        popup.classList.add("on")
     })
+
     btnClose.addEventListener("click",()=>{
-        // 팝업닫히는기능
-        popup.classList.remove("on")
         modal.classList.remove("on")
+        popup.classList.remove("on")
     })
 
     // 슬라이드
@@ -24,12 +23,12 @@ document.addEventListener("DOMContentLoaded",()=>{
     setInterval(()=>{
         count++
         train.style.transition = `all 0.7s ease 0s`
-        train.style.transform = `translateX(${-25*count}%)`
+        train.style.transform = `translateY(${-25*count}%)`
         if(count==3){
             setTimeout(()=>{
-                train.style.transition = `none`
-                train.style.transform = `translateX(0%)`
                 count=0
+                train.style.transition = `none`
+                train.style.transform = `translateY(0%)`
             },800)
         }
     },2500)
